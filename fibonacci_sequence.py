212573a56b18c0cb7071a711dc2e6fd2a71faee0
gummi <gummiht@gmail.com>
2024-01-25 11:05:47 +0000

    Update fibonacci_sequence.py

--- fibonacci_sequence.py
+++ fibonacci_sequence.py
@@ -3,8 +3,10 @@ def fibonacci(n):
     if n == 0:
         return 0
     elif n == 1:
-        return 1
+        return 1 
+    elif n < 0: 
+        return "Invalid input, please enter a positive integer"
     else: 
         return fibonacci(n-1) + fibonacci(n-2) 
     
-#print(fibonacci(8))  0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
+print(fibonacci(-1))  #0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
