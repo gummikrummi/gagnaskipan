42628b7f9e71f1da4f1ca2a658a20001cd550cf5
gummi <gummiht@gmail.com>
2024-01-30 14:14:30 +0000

    Update recursive_programming_extravaganzaII.py

--- recursive_programming_extravaganzaII.py
+++ recursive_programming_extravaganzaII.py
@@ -8,21 +8,35 @@ def length_of_string(string):
     if not string:
         return 0
     else:
-        return length_of_string(string[1:]) + 1 
+        return length_of_string(string[1:]) + 1  
+print(length_of_string("Hello World!"))
     
-def linear_search():
+def linear_search(lst, val): 
+
 #   ○ Implement a recursive function that searches for a value in a list
 #   ○ Takes a list and a single value as parameters
 #   ○ Returns a boolean value
 #       ■ True if the value is in the list, otherwise False 
-    pass 
-
-def count_instances():
+    if not lst:
+        return False 
+    elif lst[0] == val: 
+        return True 
+    else: 
+        return linear_search(lst[1:],val) 
+print(linear_search([1,2,3,4,5,6,7,8,9], 10))
+    
+def count_instances(lst, val):
 #   ○ Implement a recursive function that counts a specific value in a list
 #   ○ Takes a list and a single value as parameters
 #   ○ Returns an integer value
 #       ■ How many times does that value appear in the list? 
-    pass 
+    if not lst:
+        return 0
+    elif lst[0] == val:
+        return count_instances(lst[1:],val) + 1
+    else:
+        return count_instances(lst[1:],val) 
+print(count_instances([1,2,3,4,5,6,7,8,9], 9))
 
 def are_there_duplicates_in_a_list
 #   ○ Implement a recursive function that checks for duplicate values in a list
